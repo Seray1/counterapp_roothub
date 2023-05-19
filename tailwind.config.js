3da6615a6bacce7db33d8e2a41927b49374ad457
Seray1 <107807594+Seray1@users.noreply.github.com>
2023-05-19 21:54:17 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ["./build/*.html"],
   theme: {
     extend: {
       colors: {
-        softBlue: 'hsl(215, 51%, 70%)',
-        Cyan: 'hsl(178, 100%, 50%)',
-        VeryDarkBlueMainBG: 'hsl(217, 54%, 11%)',
-        VeryDarkBlueCardBG: 'hsl(216, 50%, 16%)',
-        VeryDarkBlueLine: 'hsl(215, 32%, 27%)',
-        White: 'hsl(0, 0%, 100%)',
+        softBlue: "hsl(215, 51%, 70%)",
+        Cyan: "hsl(178, 100%, 50%)",
+        VeryDarkBlueMainBG: "hsl(217, 54%, 11%)",
+        VeryDarkBlueCardBG: "hsl(216, 50%, 16%)",
+        VeryDarkBlueLine: "hsl(215, 32%, 27%)",
+        White: "hsl(0, 0%, 100%)",
       },
     },
   },
   plugins: [],
 }
-
